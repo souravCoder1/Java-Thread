53188721594f2e480f80c03a648e0818663b1a42
souravpal01 <souravpal7217@gmail.com>
2023-04-11 20:22:21 +0530

    ThreadDemo practice

--- src/ThreadDemo.java
+++ src/ThreadDemo.java
@@ -2,14 +2,18 @@ public class ThreadDemo { // current is main
     void show() {
         System.out.println(Thread.currentThread().getName());
         System.out.println(Thread.currentThread().getId());
-        for (int i = 0; i < 100; i++) {
-            Thread thread = new Thread(new DownloadFileTask());
-            System.out.println(Thread.currentThread().getName());
-            thread.start(); // ==> run
+
+        Thread thread = new Thread(new DownloadFileTask());
+
+        System.out.println(Thread.currentThread().getName());
+        thread.start(); // ==> run
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-//        Thread thread1 = new Thread(new DownloadFileTask());
-//        Thread thread2 = new Thread(new DownloadFileTask());
-//        Thread thread3 = new Thread(new DownloadFileTask());
-//        Thread thread4 = new Thread(new DownloadFileTask());
+
+        thread.interrupt();
     }
 }
