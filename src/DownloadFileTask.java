cef6d5a5b0d4c4d2d4caefff5dbdfd4d6f483f2b
souravpal01 <souravpal7217@gmail.com>
2023-04-11 20:28:48 +0530

    ThreadDemo practice

--- src/DownloadFileTask.java
+++ src/DownloadFileTask.java
@@ -3,11 +3,11 @@ public class DownloadFileTask implements Runnable {
     public void run() {
         System.out.println("Downloading files...."+ Thread.currentThread().getName()); // 1
 
-        for (int i = 0; i< Integer.MAX_VALUE; i++) {
-            if(Thread.currentThread().isInterrupted())
-                break;
-
-            System.out.println("Downloading byte " + i);
+        try {
+            Thread.sleep(5000); // approx  // 2
+        } catch (Exception e) {
+            System.out.println("Exception!!!!!");
+            e.printStackTrace();
         }
 
         System.out.println("Download complete: "+ Thread.currentThread().getName()); // 3
--- src/ThreadDemo.java
+++ src/ThreadDemo.java
@@ -2,18 +2,10 @@ public class ThreadDemo { // current is main
     void show() {
         System.out.println(Thread.currentThread().getName());
         System.out.println(Thread.currentThread().getId());
-
-        Thread thread = new Thread(new DownloadFileTask());
-
-        System.out.println(Thread.currentThread().getName());
-        thread.start(); // ==> run
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        for (int i = 0; i < 100; i++) {
+            Thread thread = new Thread(new DownloadFileTask());
+            System.out.println(Thread.currentThread().getName());
+            thread.start(); // ==> run
         }
-
-        thread.interrupt();
     }
 }
