aeff4f4c8d3ba671af05b21cc9c107a3ac9b0df0
souravpal01 <souravpal7217@gmail.com>
2023-04-14 20:32:30 +0530

    ThreadDemo practice

--- src/DownloadStatus.java
+++ src/DownloadStatus.java
@@ -7,6 +7,9 @@ public class DownloadStatus {
     }
 
     public void incrementTotalByte() { // shared resource
-        totalMb++;
+        totalMb++; // 3 operations
+        // 1. clone main memory to cpu (ram)
+        // 2. update the value
+        // 3. merge cpu to main memory
     }
 }
