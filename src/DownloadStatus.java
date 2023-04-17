b6ce95e47d6f5416720d6f74c9ff0d77fd3d72d1
souravpal01 <souravpal7217@gmail.com>
2023-04-17 20:23:23 +0530

    ThreadDemo practice

--- src/DownloadStatus.java
+++ src/DownloadStatus.java
@@ -1,13 +1,23 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DownloadStatus {
 
     private int totalMb = 0;
+    Lock lockResouce = new ReentrantLock();
 
     public int getTotalMb() {
         return totalMb;
     }
 
     public void incrementTotalByte() { // shared resource
-        totalMb++; // 3 operations
+        lockResouce.lock();
+        try {
+            totalMb++; // 3 operation
+
+        } finally {
+            lockResouce.unlock();
+        }
         // 1. clone main memory to thread local memory
         // 2. update the value
         // 3. merge cpu to main memory
