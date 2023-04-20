3c46d01d7f4598628f516cbc9b37f7c089ac31a9
souravpal01 <souravpal7217@gmail.com>
2023-04-20 20:40:26 +0530

    Changes by iftikharyasin

--- src/DownloadStatus.java
+++ src/DownloadStatus.java
@@ -1,11 +1,9 @@
 public class DownloadStatus {
-    private boolean isDone = false;
+    private volatile boolean isDone = false;
     private int totalMb = 0;
 
     public boolean isDone() {
-        synchronized (this) {
             return isDone;
-        }
     }
 
     public void done() {
@@ -21,10 +19,8 @@ public class DownloadStatus {
     }
 
     public void incrementTotalByte() { // shared resource
-
-        synchronized (this) {
             totalMb++;
-        }// 3 operations
+        // 3 operations
         // 1. clone main memory to thread local memory
         // 2. update the value
         // 3. merge cpu to main memory
