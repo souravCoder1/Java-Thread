45f08486ea7564b38c36701067286cf146107084
Coder <abc@cd.com>
2023-08-06 20:50:28 +0530

    add Thread Synchronized

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,51 @@
 public class Main {
+    static int count = 0;
+
+    public static synchronized void increment() {
+        count++;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Thread.activeCount());
-        System.out.println(Runtime.getRuntime().availableProcessors());
-        // 1. Main Thread
-        // 2. Background (Daemon)
-        //ThreadDemo threadDemo = new ThreadDemo();
-        //threadDemo.show(); // main thread
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("Thread 1 " + count);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    increment();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("Thread 2 " + count);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    increment();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Total Counter : " + count);
     }
 }
